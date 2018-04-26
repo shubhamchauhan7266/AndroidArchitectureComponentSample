87a8c17eda0a02a7707b4ecae0e51abd35ddf6ee
shubham chauhan <shubham.chauhan@kelltontech.com>
2018-04-26 11:02:06 +0530

    initial commit

--- app/src/main/java/com/androidarchitecturecomponentsample/activity/MainActivity.java
+++ app/src/main/java/com/androidarchitecturecomponentsample/activity/MainActivity.java
@@ -12,10 +12,8 @@ import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.androidarchitecturecomponentsample.R;
-import com.androidarchitecturecomponentsample.interfaces.OnResponseListener;
 import com.androidarchitecturecomponentsample.utils.AppUrl;
 import com.androidarchitecturecomponentsample.volley.VolleySingleton;
-import com.google.gson.JsonObject;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -23,13 +21,54 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * @author Shubham Gupta
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        JSONObject  params = new JSONObject();
+
+        onJsonRequest();
+    }
+
+    /**
+     * Method is used to getProduct list from server.
+     */
+    public void onJsonRequest() {
+        final JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, AppUrl.PRODUCT_DETAILS_URL, getJsonPayload(), new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.e("Dupont", response.toString());
+                Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_LONG).show();
+            }
+
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d("Inside REDCLUBAPP", "Error: " + error.getMessage());
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                return getHeader();
+            }
+        };
+
+        VolleySingleton.getInstance(this).add(jsonRequest);
+    }
+
+    /**
+     * Method is used to get Json Payload for Product Api.
+     *
+     * @return jsonPayload
+     */
+    public JSONObject getJsonPayload() {
+        JSONObject params = new JSONObject();
         try {
             params.put("searchSelectedBrand", "");
             params.put("searchsSlectedCrop", "");
@@ -43,36 +82,18 @@ public class MainActivity extends AppCompatActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+        return params;
+    }
 
+    /**
+     * Method is used to get header for Product Api.
+     *
+     * @return header
+     */
+    public HashMap<String, String> getHeader() {
         HashMap<String, String> headerHashMap = new HashMap<>();
         headerHashMap.put("Content-Type", "application/json");
         headerHashMap.put("AuthToken", "lW_LActKsl9_VSB1LbDYPG");
-        onJsonRequest(params, headerHashMap);
-
-    }
-
-    public void onJsonRequest(final JSONObject params, final HashMap<String, String> headerHashMap) {
-        final JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, AppUrl.PRODUCT_DETAILS_URL,params , new Response.Listener<JSONObject>() {
-
-            @Override
-            public void onResponse(JSONObject response) {
-                Log.e("Dupont", response.toString());
-                Toast.makeText(MainActivity.this,response.toString(),Toast.LENGTH_LONG).show();
-            }
-
-        }, new Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                VolleyLog.d("Inside REDCLUBAPP", "Error: " + error.getMessage());
-            }
-        }) {
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                return headerHashMap;
-            }
-        };
-
-        VolleySingleton.getInstance(this).add(jsonRequest);
+        return headerHashMap;
     }
 }
--- app/src/main/java/com/androidarchitecturecomponentsample/interfaces/OnResponseListener.java
+++
@@ -1,5 +0,0 @@
-package com.androidarchitecturecomponentsample.interfaces;
-
-public interface OnResponseListener {
-    public  void onResponse(Object response);
-}
