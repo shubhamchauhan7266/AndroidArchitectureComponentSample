756187a97df49abf55985dc91cba1e763f4f2cb2
Shubham Gupta <shubham.gupta1@kelltontech.com>
2018-04-26 15:07:12 +0530

    + implement the Recycler View Adapter  for Product details , create the UI and inflate it

--- app/src/main/java/com/androidarchitecturecomponentsample/interfaces/OnItemClickListener.java
+++ app/src/main/java/com/androidarchitecturecomponentsample/interfaces/OnItemClickListener.java
@@ -1,5 +1,9 @@
 package com.androidarchitecturecomponentsample.interfaces;
 
+/**
+ * @author :Shubham Gupta
+ * this method is used to  handle the click listeners
+ */
 public interface OnItemClickListener {
     void onItemClick(int postion);
     void onItemLongPressedListener(int position);
